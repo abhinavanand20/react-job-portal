33c1f6d8e46ea9a51997dd07d042ca3489d534ca
Akshita <akshitashree1910@gmail.com>
2026-06-28 13:42:16 +0530

    Initial commit

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import {Context} from "../../main"
+import {Link} from "react-router-dom"
+import { FaGithub , FaLinkedin} from "react-icons/fa"
+import { SiLeetcode } from "react-icons/si";
+import { RiInstagramFill} from "react-icons/ri"
+function Footer() {
+  const {isAuthorized}  = useContext(Context)
+  return (
+    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
+<div>&copy; All Rights Reserved by Abhinav.</div>
+<div>
+  <Link to={'https://github.com/abhinavanand20'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://leetcode.com/u/abhinavanand20/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
+  <Link to={'https://www.linkedin.com/in/abhinav-anand-140500251/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'https://www.instagram.com/abhinavanand20/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+</div>
+      
+    </footer>
+  )
+}
+
+export default Footer
